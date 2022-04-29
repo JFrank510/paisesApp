54ff9d63be73caa9ec5a4702ab75d52f872c23cf
JoseFranciscoPeraltaRamos <49628919+JFrankgod@users.noreply.github.com>
2022-04-28 21:48:43 -0500

    Implementacion terminada

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,19 +12,23 @@ export class PorPaisComponent {
 
   termino  : string = '';
   hayError : boolean = false;
+  paises   : Country[] = [];
   constructor(private paisService:PaisService) { }
 
   buscar(){
     this.hayError = false;
     console.log(this.termino);
-    
+
     this.paisService.searchPais(this.termino).
     subscribe((resp) =>{ 
       console.log(resp);
+      this.paises = resp;
     }, (err) =>{
       this.hayError = true;
+      this.paises = [];
     });
     
   }
+
 
 }
